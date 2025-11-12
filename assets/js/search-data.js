94e8316d77d4df2ddc0094762ceffeabed6f3281
rfangit <77313639+rfangit@users.noreply.github.com>
2025-11-12 02:30:25 +0000

    Deploying to gh-pages from @ rfangit/rfangit.github.io@23c94b7d2c577ebf96e1cc1470ea1e827c2138ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-introduction-to-mean-flow",
+        },{id: "post-the-volume-hypothesis-and-sharp-minima",
+        
+          title: "The Volume Hypothesis And Sharp Minima",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/minima_volumes_sharp/";
+          
+        },
+      },{id: "post-introduction-to-mean-flow",
         
           title: "Introduction to Mean Flow",
         
